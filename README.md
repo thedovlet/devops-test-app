7b2c76f3785b4e7c09b02142e65a3fb65c878e98
thedovlet <tmgameer04@gmail.com>
2025-08-22 21:47:51 +0000

    Добавлен README, мониторинг, .env.example и тесты

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# devops-test-app
-This is devops test project for emekdep
+# Getting Started App
+
+This repository contains a Node.js application with a PostgreSQL database, Redis cache, and monitoring using Prometheus & Grafana. The app is fully dockerized and has a CI/CD pipeline set up via GitHub Actions.
+
+---
+
+## Table of Contents
+
+1. [Running the App](#running-the-app)  
+2. [Environment Variables](#environment-variables)  
+3. [CI/CD Pipeline](#cicd-pipeline)  
+4. [Monitoring & Grafana](#monitoring--grafana)  
+5. [Credentials & Ports](#credentials--ports)  
+
+---
+
+## Running the App
+
+You can run the application and all services with Docker Compose:
+
+```bash
+git clone https://github.com/thedovlet/devops-test-app.git
+cd getting-started-app
+docker-compose up -d
--- src/index.js
+++ src/index.js
@@ -1,32 +1,62 @@
 const express = require('express');
-const app = express();
+const client = require('prom-client');
 const db = require('./persistence');
 const getItems = require('./routes/getItems');
 const addItem = require('./routes/addItem');
 const updateItem = require('./routes/updateItem');
 const deleteItem = require('./routes/deleteItem');
 
+const app = express();
 app.use(express.json());
 app.use(express.static(__dirname + '/static'));
 
+// REST API маршруты
 app.get('/items', getItems);
 app.post('/items', addItem);
 app.put('/items/:id', updateItem);
 app.delete('/items/:id', deleteItem);
 
-db.init().then(() => {
-    app.listen(3000, () => console.log('Listening on port 3000'));
-}).catch((err) => {
-    console.error(err);
-    process.exit(1);
+// --- Prometheus metrics ---
+const register = new client.Registry();
+client.collectDefaultMetrics({ register });
+
+const httpRequestDurationMicroseconds = new client.Histogram({
+  name: 'http_request_duration_ms',
+  help: 'Duration of HTTP requests in ms',
+  labelNames: ['method', 'route', 'code'],
+  buckets: [50, 100, 200, 300, 400, 500, 1000, 2000]
+});
+register.registerMetric(httpRequestDurationMicroseconds);
+
+// middleware для измерения времени ответа
+app.use((req, res, next) => {
+  const end = httpRequestDurationMicroseconds.startTimer();
+  res.on('finish', () => {
+    end({ method: req.method, route: req.path, code: res.statusCode });
+  });
+  next();
 });
 
-const gracefulShutdown = () => {
-    db.teardown()
-        .catch(() => {})
-        .then(() => process.exit());
-};
+// точка для Prometheus
+app.get('/metrics', async (req, res) => {
+  res.setHeader('Content-Type', register.contentType);
+  res.end(await register.metrics());
+});
 
-process.on('SIGINT', gracefulShutdown);
-process.on('SIGTERM', gracefulShutdown);
-process.on('SIGUSR2', gracefulShutdown); // Sent by nodemon
+// --- запуск сервера ---
+db.init()
+  .then(() => {
+    app.listen(3000, () => console.log('Listening on port 3000'));
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
+
+// graceful shutdown
+const gracefulShutdown = () => {
+  db.teardown()
+    .catch(() => {})
+    .then(() => process.exit());
+};
+process.on('SIGINT', gracefulShutdow
